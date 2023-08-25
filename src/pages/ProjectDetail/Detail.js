476daf4412820aceca950bb6994cd7fab9797615
zunyange <hiillun00553300@gmail.com>
2023-08-25 23:37:09 +0900

    ADD : Start to Contact

---
+++ src/pages/ProjectDetail/Detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProjectDetail from './ProjectDetail/ProjectOne';
+
+const Detail = () => {
+  return (
+    <div>
+      <ProjectDetail />
+    </div>
+  );
+};
+
+export default Detail;
