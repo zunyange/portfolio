27e00b93314a2df9cd86001e74f68809a9de9e1c
zunyange <hiillun00553300@gmail.com>
2023-09-01 13:07:40 +0900

    SOLVE : navToggleRef

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './ContactStyled.js';
+import { Width } from '../../styles/common.js';
+
+const Contact = ({ about }) => {
+  return (
+    <S.Contact>
+      <Width>dd</Width>
+    </S.Contact>
+  );
+};
+
+export default Contact;
