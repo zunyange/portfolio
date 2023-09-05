964ef9d27140fb5adbff70ffff6c1a891361de73
zunyange <hiillun00553300@gmail.com>
2023-09-06 02:06:33 +0900

    Contact Layout

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,7 +8,37 @@ const Contact = ({ contact }) => {
       <Width>
         <S.ContactContainer>
           <S.Title>Let's Get in Touch</S.Title>
-          <S.ContactWrap>-</S.ContactWrap>
+          <S.ContactWrap>
+            <S.GoTo>
+              <a
+                href="https://github.com/zunyange"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <S.GoToGithub src="/images/icon/github.png" alt="Github" />
+              </a>
+              <a
+                href="https://velog.io/@zunyange"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <S.GoToVelog src="/images/icon/velog.png" alt="Velog" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/junyoung-kim-067765281/"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <S.GoToLinkedin
+                  src="/images/icon/linkedin.png"
+                  alt="Linkedin"
+                />
+              </a>
+              <a href="mailto:giveyoung530@gmail.com">
+                <S.GoToMail src="/images/icon/send.png" alt="Gmail" />
+              </a>
+            </S.GoTo>
+          </S.ContactWrap>
         </S.ContactContainer>
       </Width>
     </S.Contact>
--- src/components/Contact/ContactStyled.js
+++ src/components/Contact/ContactStyled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Contact = styled.div`
-  height: 30vh;
+  height: 38vh;
   width: 100%;
   background-color: #f5fbff;
   @media ${({ theme }) => theme.device.tablet} {
@@ -24,10 +24,80 @@ export const ContactContainer = styled.div`
 export const Title = styled.div`
   font-family: fontEng;
   font-size: 40px;
-  margin: 70px 0;
+  margin: 70px 0 40px 0;
   @media ${({ theme }) => theme.device.tablet} {
     margin-top: 70px;
   }
 `;
 
 export const ContactWrap = styled.div``;
+
+export const GoTo = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 35px;
+  @media ${({ theme }) => theme.device.mobile} {
+    gap: 20px;
+  }
+`;
+
+export const GoToGithub = styled.img`
+  width: 50px;
+  cursor: pointer;
+  transition: 0.7s;
+  &:hover {
+    filter: invert(100%);
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 40px;
+  }
+`;
+
+export const GoToBlack = styled.img`
+  width: 50px;
+  display: none;
+  ${GoTo} :hover & {
+    display: block;
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 40px;
+  }
+`;
+
+export const GoToVelog = styled.img`
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  transition: 0.7s;
+  &:hover {
+    filter: invert(100%);
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 40px;
+    height: 40px;
+  }
+`;
+
+export const GoToLinkedin = styled.img`
+  width: 50px;
+  cursor: pointer;
+  transition: 0.7s;
+  &:hover {
+    filter: invert(100%);
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 40px;
+  }
+`;
+
+export const GoToMail = styled.img`
+  width: 55px;
+  cursor: pointer;
+  transition: 0.7s;
+  &:hover {
+    filter: invert(100%);
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 40px;
+  }
+`;
